00f055c352f3820aa92a6e38ca17ff754463f2fe
wangcailin <dreamcailin@hotmail.com>
2018-01-03 16:10:59 +0800

    归档

--- application/admin/lang/zh-cn/user.php
+++ application/admin/lang/zh-cn/user.php
@@ -3,15 +3,13 @@
 return [
     'Username'  =>  '用户名',
     'Userpass'  =>  '密码',
-    'Nickname'  =>  '用户昵称',
-    'Avatar'  =>  '用户头像',
+    'Nickname'  =>  '昵称',
+    'Avatar'  =>  '头像',
     'Mobile'  =>  '手机号',
-    'Vip'  =>  '会员',
+    'Vip'  =>  '会员等级: 1普通会员,2VIP,3SVIP',
     'Vip_time'  =>  '会员到期时间',
-    'Company'  =>  '公司名称',
-    'Card'  =>  '身份证',
-    'Realname'  =>  '真实姓名',
     'Create_time'  =>  '注册时间',
     'Update_time'  =>  '上次登录时间',
-    'Status'  =>  '状态'
+    'Open_id'  =>  'Openid',
+    'Status'  =>  '状态: normal正常,hidden隐藏'
 ];
--- public/assets/js/backend/user.js
+++ public/assets/js/backend/user.js
@@ -26,14 +26,15 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                         {checkbox: true},
                         {field: 'id', title: __('Id')},
                         {field: 'username', title: __('Username')},
+                        {field: 'userpass', title: __('Userpass')},
                         {field: 'nickname', title: __('Nickname')},
-                        {field: 'avatar', title: __('Avatar'), formatter: Table.api.formatter.image},
+                        {field: 'avatar', title: __('Avatar')},
                         {field: 'mobile', title: __('Mobile')},
-                        {field: 'vip', title: __('Vip'), formatter: Controller.api.formatter.vip},
+                        {field: 'vip', title: __('Vip')},
                         {field: 'vip_time', title: __('Vip_time'), formatter: Table.api.formatter.datetime},
-                        {field: 'realname', title: __('Realname')},
                         {field: 'create_time', title: __('Create_time'), formatter: Table.api.formatter.datetime},
                         {field: 'update_time', title: __('Update_time'), formatter: Table.api.formatter.datetime},
+                        {field: 'open_id', title: __('Open_id')},
                         {field: 'status', title: __('Status'), formatter: Table.api.formatter.status},
                         {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
                     ]
@@ -52,15 +53,6 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
         api: {
             bindevent: function () {
                 Form.api.bindevent($("form[role=form]"));
-            },
-            formatter: {
-                vip: function (value, row, index) {
-                    if (value == 0){
-                        return '<span class="label label-danger">否</span>';
-                    }else{
-                        return '<span class="label label-warning">是</span>';
-                    }
-                }
             }
         }
     };
