2c69721acfc5a858667bd618ffac7f845c389990
wangcailin <dreamcailin@hotmail.com>
2018-01-07 18:24:40 +0800

    归档

--- application/api/controller/Wechat.php
+++ application/api/controller/Wechat.php
@@ -26,7 +26,7 @@ class Wechat extends Api
     {
         $jssdkObj = new JSSDK("wxa0afc75ebe2d5871", "75cbdd6b7e9b58e90f1c5e8c8de802f9");
         $signPackage = $jssdkObj->getSignPackage();
-        $this->signPackage = $signPackage;
+        $this->assign('signPackage', $signPackage);
     }
 
     public function checktoken(){
@@ -46,6 +46,41 @@ class Wechat extends Api
         }else{
             return false;
         }
+    }
 
+    public function getCode(){
+        $appid = 'wxa0afc75ebe2d5871';
+        $secret = '75cbdd6b7e9b58e90f1c5e8c8de802f9';
+        $code = $_GET['code'];
+        $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$secret&code=$code&grant_type=authorization_code";
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$url );
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1 );
+        $res = curl_exec($ch);
+        curl_close($ch);
+
+        $arr = json_decode($res , true);
+        $this -> getUserInfo($arr["access_token"],$arr["openid"]);
+    }
+
+
+    public function getUserInfo($accessToken , $openid){
+        $url = "https://api.weixin.qq.com/sns/userinfo?access_token=$accessToken&openid=$openid&lang=zh_CN";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$url );
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1 );
+        $res = curl_exec($ch);
+        curl_close($ch);
+        $arr = json_decode($res , true);
+        echo "<h1>用户名：".$arr['nickname']."</h1>";
+        echo "<h1>头像：<img style='width: 10%;text-align: center' src=".$arr['headimgurl']."></h1>";
+        if($arr['sex'] == '1'){
+            $arr['sex'] = '男';
+        }else{
+            $arr['sex'] ='女';
+        }
+        echo "<h2>性别：".$arr['sex']."</h2>";
+        echo "<h3>国家：".$arr['country']."</h3>";
     }
 }
