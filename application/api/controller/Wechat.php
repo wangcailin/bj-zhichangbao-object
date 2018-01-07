34f29f3d025021538932e8615f64bac5c1ef38d8
wangcailin <dreamcailin@hotmail.com>
2018-01-07 17:33:15 +0800

    归档

--- application/api/controller/Wechat.php
+++ application/api/controller/Wechat.php
@@ -5,6 +5,8 @@ namespace app\api\controller;
 use app\common\controller\Api;
 use wechat\js\JSSDK;
 use think\Config;
+use think\Request;
+
 /**
  * 微信
  *
@@ -25,5 +27,25 @@ class Wechat extends Api
         $jssdkObj = new JSSDK("wxa0afc75ebe2d5871", "75cbdd6b7e9b58e90f1c5e8c8de802f9");
         $signPackage = $jssdkObj->getSignPackage();
         $this->signPackage = $signPackage;
+    }
+
+    public function checktoken(){
+        $request = Request::instance();
+        $get = $request->param();
+        $timestamp = $get['timestamp'];//timestamp其实就是一个时间戳
+        $nonce     = $get['nonce'];//nonce是一个随机参数
+        $token     = "zcb357951";//这个token填写你在微信公众平台上写的那个值
+        $signature = $get['signature'];//这个signature其实就是在微信公众平台已经加密好的字符串
+        $echostr   = $get['echostr'];
+        $array = [];
+        $array = array($token,$timestamp,$nonce);
+        sort($array);
+        $tmpstr = sha1(implode('',$array));
+        if($tmpstr == $signature){
+            echo $get['echostr'];
+        }else{
+            return false;
+        }
+
     }
 }
