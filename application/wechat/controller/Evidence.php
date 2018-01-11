87b0c01d64edf692aad1b36421ac8270197b2d4c
wangcailin <dreamcailin@hotmail.com>
2018-01-11 19:09:25 +0800

    .

--- application/wechat/controller/Evidence.php
+++ application/wechat/controller/Evidence.php
@@ -43,7 +43,54 @@ class Evidence extends Wechat
 
         }
         return $this->view->fetch();
+    }
 
+    public function addCheck()
+    {
+        $user_id            = input('user_id');
+        $company            = input('company');
+        $company_address    = input('company_address');
+        $contact            = input('contact');
+        $contact_phone      = input('contact_phone');
+        $job_address        = input('job_address');
+        $job_post           = input('job_post');
+        $job_time           = input('job_time');
+        $compact            = input('compact');
+        $rights_type        = input('rights_type');
+        $rights_money       = input('rights_money');
+        $email              = input('email');
+        $description        = input('description');
+        $ask                = input('ask');
+        $job_name           = input('job_name');
+        $job_age            = input('job_age');
+        $sex                = input('sex');
+        $registered         = input('registered');
+        $data = [
+            'evidence_sn'      => date('Ymd', time()),
+            'user_id'          => $user_id,
+            'company'          => $company,
+            'company_address'  => $company_address,
+            'contact'          => $contact,
+            'contact_phone'    => $contact_phone,
+            'job_address'      => $job_address,
+            'job_post'         => $job_post,
+            'job_time'         => $job_time,
+            'compact'          => $compact,
+            'rights_type'      => $rights_type,
+            'rights_money'     => $rights_money,
+            'email'            => $email,
+            'description'      => $description,
+            'ask'              => $ask,
+            'job_name'         => $job_name,
+            'job_age'          => $job_age,
+            'sex'              => $sex,
+            'registered'       => $registered
+        ];
+        if ($this->model->save($data)) {
+            return 1;
+        }else{
+            return 0;
+        }
     }
 
 
