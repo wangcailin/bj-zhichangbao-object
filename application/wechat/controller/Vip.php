9faed45aa259696ad39fb084ee0f48976c3e74c2
wangcailin <dreamcailin@hotmail.com>
2018-01-15 21:53:22 +0800

    debug

--- application/wechat/controller/Vip.php
+++ application/wechat/controller/Vip.php
@@ -133,12 +133,12 @@ class Vip extends Wechat
         $response = $this->wxPay->handlePaidNotify(function($message, $fail){
             $out_trade_no = $message['out_trade_no'];
             $order_info = $this->orderModel->where('order_sn', $out_trade_no)->find();
-
+            @file_put_contents('fail.txt', json_encode($order_info));
+            @file_put_contents('notify.txt',json_encode($message));
             if(empty($order_info)){
                 return false;
             }
-            @file_put_contents('fail.txt', json_encode($order_info));
-            @file_put_contents('notify.txt',json_encode($message));
+
             if($order_info['pay_time']>0){
                 return true;
             }
