e6983630eaee1430006d2e0c00999c06c1e0c4ca
wangcailin <dreamcailin@hotmail.com>
2018-01-22 21:48:54 +0800

    debug

--- application/wechat/controller/Active01.php
+++ application/wechat/controller/Active01.php
@@ -3,12 +3,27 @@
 namespace app\wechat\controller;
 
 use app\common\controller\Api;
+use EasyWeChat\Factory;
 
 class Active01 extends Api
 {
+    private $wxPay = null;
+    private $orderModel = null;
     public function _initialize()
     {
         parent::_initialize();
+        $this->orderModel = model('Order');
+        $payConfig = [
+            // 必要配置
+            'app_id'             => 'wxa0afc75ebe2d5871',
+            'secret'             => '75cbdd6b7e9b58e90f1c5e8c8de802f9',
+            'mch_id'             => '1401831202',
+            'key'                => 'BGQv5ebUj5Ug8FLJMyPg8ZvKoRxqYMlf',
+            'cert_path'          => 'wxpay/apiclient_cert.pem',
+            'key_path'           => 'wxpay/apiclient_key.pem',
+            'notify_url'         => 'http://www.zhichangbb.com/wechat/vip/notify',
+        ];
+        $this->wxPay = Factory::payment($payConfig);
     }
 
     /**
@@ -64,5 +79,84 @@ class Active01 extends Api
             $data['code'] = 1;
         }
         return json($data);
+    }
+
+    /**
+     * 冲一年会员送一年视频会员
+     */
+    public function vip_video()
+    {
+        $user_id = session('user_id');
+        $wechat_user = session('wechat_user');
+
+        $out_trade_no = date('Ymd', time()).rand(10000,99999);
+        $order = [
+            'body' => '职场保-包年会员-充值',
+            'out_trade_no' => $out_trade_no,
+            'total_fee' => 39.6*100,
+            'trade_type' => 'JSAPI',
+            'openid' => $wechat_user['openid'],
+        ];
+
+        $result = $this->wxPay->order->unify($order);
+
+        if ($result['return_code'] == 'SUCCESS' && $result['result_code'] == 'SUCCESS'){
+            $prepayId = $result['prepay_id'];
+
+            $this->orderModel->order_sn     = $out_trade_no;
+            $this->orderModel->vid          = 1;
+            $this->orderModel->prepay_id    = $prepayId;
+            $this->orderModel->user_id      = $user_id;
+            $this->orderModel->goods_name   = '包年荣誉会员';
+            $this->orderModel->amount       = 39.6;
+            $this->orderModel->status       = 0;
+            $this->orderModel->add_time     = time();
+            $this->orderModel->save();
+            if (!$this->orderModel->id){
+                $this->error('生成订单失败!', '/wechat/vip/index');
+            }
+        }
+        $json = $this->wxPay->jssdk->bridgeConfig($prepayId);
+        $conf = $this->app->jssdk->buildConfig(array('translateVoice','chooseWXPay','getBrandWCPayRequest','onMenuShareTimeline', 'onMenuShareAppMessage'), false, false, true);
+        $this->assign(['jsorder' => $json,'conf' => $conf,'order_sn' => $out_trade_no]);
+        return $this->view->fetch();
+    }
+
+    /**
+     * 活动支付回调
+     * @return mixed
+     */
+    public function notify(){
+        $response = $this->wxPay->handlePaidNotify(function($message, $fail){
+            $out_trade_no = $message['out_trade_no'];
+            $order_info = model('Order')->where('order_sn', $out_trade_no)->find();
+            $order_info->pay_type = 1;
+            @file_put_contents('fail.txt', json_encode($order_info));
+            @file_put_contents('notify.txt',json_encode($message));
+            if(empty($order_info)){
+                return false;
+            }
+            if($order_info['pay_time']>0){
+                return true;
+            }
+            if($message['return_code'] === 'SUCCESS'){
+                // 用户是否支付成功
+                if ($message['result_code'] === 'SUCCESS') {
+                    $order_info->transaction_id = $message['transaction_id'];
+                    $order_info->status         = 1;
+                    $order_info->pay_time       = time();
+                }
+            }else{
+                return $fail('通信失败，请稍后再通知我');
+            }
+            if($order_info->save()){
+                $this->add_vip($order_info);
+                return true;
+            }else{
+                return false;
+            }
+            return true; // 返回处理完成
+        });
+        $response->send(); // return $response;
     }
 }
--- application/wechat/controller/User.php
+++ application/wechat/controller/User.php
@@ -48,7 +48,7 @@ class User extends Wechat
             $this->model->save($data);
             session('user_id', $res->id);
         }
-        header("Location:http://www.zhichangbb.com/wechat/vip/vip_video");
+        header("Location:http://www.zhichangbb.com/wechat/index/index");
     }
 
     public function getAjaxUserInfo()
